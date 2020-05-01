75269abe58b25922863c4ee515be8bdf41039a6b
Grace Cho <ekcho3650@gmail.com>
2020-04-30 23:46:43 -0400

    fixed indentation

--- client/src/components/MyOrder.js
+++ client/src/components/MyOrder.js
@@ -14,40 +14,40 @@ const MyOrder = (props) => {
 
    const getProducts = async() => {
     //async를 쓰면 순차적처리가 아니라 동시처리로 바뀌고 await을 쓰면 동시처리 하지말고 await사용한 줄은 기다리고 그 다음줄부터 사용
-   return await Axios.get("/products").then((res)=>{
-        console.log('res.body', res.data);
-        const products = {};
-        res.data.forEach(item=>{
-            products[item._id] = item.name;
-        });
-        return products;
-    });     
-};
+    return await Axios.get("/products").then((res)=>{
+            console.log('res.body', res.data);
+            const products = {};
+            res.data.forEach(item=>{
+                products[item._id] = item.name;
+            });
+            return products;
+        });     
+    };
 
-const getOrders = async() => {
-    const products = await getProducts();
+    const getOrders = async() => {
+        const products = await getProducts();
 
-    Axios.get("/orders").then((res)=>{
-        console.log('res.body orders', res.data);
-        const customers = {};
-        
-        res.data.forEach(item=>{
-            item.productName = products[item.productId];       
-        })
-        console.log('original orders', res.data)
-        console.log('customer name', props.customerName);
-        console.log('this customers orders are', res.data.filter((item)=>(item.customerName === props.customerName)))
-        // setOrders(res.data.filter((item)=>(item.customerName === props.customerName)));
-        setOrders(res.data);
+        Axios.get("/orders").then((res)=>{
+            console.log('res.body orders', res.data);
+            const customers = {};
+            
+            res.data.forEach(item=>{
+                item.productName = products[item.productId];       
+            })
+            console.log('original orders', res.data)
+            console.log('customer name', props.customerName);
+            console.log('this customers orders are', res.data.filter((item)=>(item.customerName === props.customerName)))
+            // setOrders(res.data.filter((item)=>(item.customerName === props.customerName)));
+            setOrders(res.data);
 
-    }); 
-}; 
+        }); 
+    }; 
 
-const onClickDelete = (orderId) => {
-    Axios.delete("/orders/"+orderId).then((res)=>{
-        getOrders();
-    }) 
-}
+    const onClickDelete = (orderId) => {
+        Axios.delete("/orders/"+orderId).then((res)=>{
+            getOrders();
+        }) 
+    }
 
     useEffect(()=>{
         getOrders();
